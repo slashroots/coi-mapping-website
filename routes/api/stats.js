9cbdc170a693d6c5890589441a9791735e7eedf2
tremainebuchanan <tremainekbuchanan@gmail.com>
2015-12-14 16:20:39 -0500

    Statistics Update Changed the behaviour of the stats endpoint to provide granular stats for the map view. An object is now returned with a count of all countries, stakeholders at a global, national and regional level.

--- routes/api/stats.js
+++ routes/api/stats.js
@@ -2,32 +2,83 @@
  * Created by tremaine on 12/11/15.
  */
 (function(){
-	var keystone = require('keystone'),
-		Country = keystone.list('Country'),
-		Stakeholder = keystone.list('Stakeholder'),
-		common = require('../common/common'),
-		ObjectId = require('mongoose').Types.ObjectId;
+var keystone = require('keystone'),
+	Country = keystone.list('Country'),
+	Stakeholder = keystone.list('Stakeholder'),
+	common = require('../common/common');
+/**
+ * 
+ * @param req
+ * @param res
+ */
+exports.index = function(req, res){
+	if(req.query.type){
 		
-	
-	exports.index = function(req, res){
-		//Country.model.findOne()
-		//	.where('name', 'Global')
-		//	.exec(function(err, country){
-		//		if(err) common.handleDBError(err, res);
-		//	
-		//		console.log(country);
-			Stakeholder.model.find({country: "565b59aa7631649209bc2058"})
-				//.where('country', country.id) Returns a string representing the ObjectId
-				//.where('country', country._id) //Returns an object of type ObjectId
-				//.where('country.name', 'Global')
-				.populate('country')
-				.exec(function(err, stake){
-				
-				if(err) common.handleDBError(err, res);
-    
-				res.json(stake);
-			});			
-		//});		
-	};
+	}else{
+		Country.model.find({"$or": [{name: 'Global'}, {name: 'Regional'}]})
+			.where('state', 'published')
+			.sort('name')
+			.exec(function(err, region){
+				if(err) common.handleDBError(err, res);				
+				/**
+				 * Get all global stakeholders
+				 */
+				Stakeholder.model
+					.find()
+					.count({country: region[0].id})
+					.exec(function(err, global_stakes){
+						if(err || !global_stakes) common.handleDBError(err, res);
+						/**
+						 * Get all regional stakeholders
+						 */
+						Stakeholder.model
+							.find({country: region[1].id})
+							.count()
+							.exec(function(err, regional_stakes){
+								if(err || !regional_stakes){
+									common.handleDBError(err, res);
+								}else{
+									/**
+									 * Get all country level stakeholders
+									 */
+									Stakeholder.model
+											  .find()
+												.count({"$and":
+												[
+													{country: { "$ne": region[0].id}},
+													{country: { "$ne": region[1].id}}
+												]
+												}).exec(function(err, national){
+													if(err || !national){
+														common.handleDBError(err, res);								
+													}else{
+														/**
+														 * Count all countries except Regional 
+														 * and Global.
+ 														 */
+														Country.model
+															   .find()
+																.count({"$and":
+																	[
+																		{name: {"$ne": 'Global'}},
+																		{name: {"$ne": 'Regional'}}
+																	]
+																}).exec(function(err, country){
+																	var stats = {};
+																	stats.global = global_stakes;
+																	stats.regional = regional_stakes;
+																	stats.national = national;
+																	stats.country = country;
+																	res.json(stats);
+														   });														     
+													}
+												});
+									
+								}
+							})							
+					});
+		});
+	}					
+};
 })();
 
