a243d08b1e473e27d1db49c9e45fa149de6cf3a4
Nick Williams <nick22891@yahoo.com>
2016-03-18 12:38:48 -0500

    Small optimisations and removing unnecessary code we no longer use as well as extra \n characters.

--- public/js/map-js.js
+++ public/js/map-js.js
@@ -1,77 +1,42 @@
 var geocodesData = null;
-
 var stakeholderCategories = null;
-
 var initiativeCategories = null;
-
 var mapLoaded = false;
-
 var size = 0;
-
 $.ajax({
-
 	type: "GET",
-
 	url: "/countries",
-
 	dataType: "json",
-
 	success: function (data) {
-
 		geocodesData = data;
-		
 		loadCountries();
-		
 		mapReady();
-		
 		mapLoaded = true;
-
 	}
-
 });
 
 $.ajax({
-
 	type: "GET",
-
-	//url: encodeURI("http://localhost:3000/countries"),
 	url: "/categories?category=stakeholder",
-
 	dataType: "json",
-
 	success: function (data) {
-
 		stakeholderCategories = data;
-
 		loadStakeholderCategories ();
-
 	}
-
 });
 
 $.ajax({
-
 	type: "GET",
-
-	//url: encodeURI("http://localhost:3000/countries"),
 	url: "/categories?category=initiative",
-
 	dataType: "json",
-
 	success: function (data) {
-
 		initiativeCategories = data;
-
 		loadInitiativeCategories ();
-
 	}
-
 });
 
 function clusterClickHandler (event) {
-
 	var ctryname = event.target.ctryname;
-
 	var popup = L.popup(
 		{
 
@@ -86,61 +51,38 @@ function clusterClickHandler (event) {
 }
 
 var countryArr = [];
-
 var stakeholderCatArr = [];
-
 var initiativeCatArr = [];
 
 function loadStakeholderCategories () {
-
 	for (var i = 0; i < stakeholderCategories.length; i++) {
-
 		var scname = stakeholderCategories[i].name;
-
 		stakeholderCatArr.push(scname);
-
 		scname = scname.replace(/\W/g, '');
-		
 		window[scname + "Toggle"] = false;
-
 	}//endfor
-	
 }
 
 function loadInitiativeCategories () {
-
 	for (var i = 0; i < initiativeCategories.length; i++) {
-
 		var icname = initiativeCategories[i].name;
-		
 		initiativeCatArr.push(icname);
-
 		icname = icname.replace(/\W/g, '');
-
 		window[icname + "Toggle"] = false;
-
 	}//endfor
-
 }
 
 function loadCountries () {
-
 		for (var i = 0; i < geocodesData.length; i++) {
-
 			var cname = geocodesData[i].name;
-
 			cname = cname.toLowerCase();
-
 			if (cname.split(" ")[0] == "saint") {
-
 				cname = cname.replace(" ", "");
-
 			}
-
 			else cname = cname.split(" ")[0];
 
 			if (!mapLoaded) countryArr.push(cname);
-
+			
 			window[cname] = new L.MarkerClusterGroup({
 				maxClusterRadius: 60,
 				iconCreateFunction: null,
@@ -151,9 +93,7 @@ function loadCountries () {
 			});
 
 			window[cname].ctryname = cname;
-
 			window[cname].on('clusterclick', clusterClickHandler);
-
 			window[cname + 'initiatives'] = new L.MarkerClusterGroup({
 				maxClusterRadius: 60,
 				iconCreateFunction: null,
@@ -162,90 +102,55 @@ function loadCountries () {
 				singleMarkerMode: true,
 				zoomToBoundsOnClick: false,
 			});
-
 			window[cname + 'initiatives'].ctryname = cname + 'initiatives';
-
 			window[cname + 'initiatives'].on('clusterclick', clusterClickHandler);
-
 			window[cname + 'PopupText'] = "<b><p style='font-size:11pt;border-bottom: 1px solid #000;margin:0;padding:0;'>Organizations</p></b>";
-
 			window[cname + 'initiativesPopupText'] = "<b><p style='font-size:11pt;border-bottom: 1px solid #000;margin:0;padding:0;'>Initiatives</p></b>";
 
 			if (!mapLoaded) window[cname + 'Toggle'] = false;
-
 		}//endfor
 		
 	globalPopupText = "<b><p style='font-size:11pt;border-bottom: 1px solid #000;margin:0;padding:0;''>Global Organizations</p></b>";
-
 	regionalPopupText = "<b><p style='font-size:11pt;border-bottom: 1px solid #000;margin:0;padding:0;''>Regional Organizations</p></b>";
-
 	globalinitiativesPopupText = "<b><p style='font-size:11pt;border-bottom: 1px solid #000;margin:0;padding:0;''>Global Initiatives</p></b>";
-
 	regionalinitiativesPopupText = "<b><p style='font-size:11pt;border-bottom: 1px solid #000;margin:0;padding:0;''>Regional Initiatives</p></b>";
 
 	if (stakeholdersToggle) {
-
 		drawLabels();
 
 		for (var i = 0; i < geocodesData.length; i++) {
-			
 			var cname = geocodesData[i].name;
-
 			cname = cname.toLowerCase();
-
 			if (cname.split(" ")[0] == "saint") {
-
 				cname = cname.replace(" ", "");
-
 			}
-
 			else cname = cname.split(" ")[0];
-
 			map.addLayer(window[cname]);
-			
 		}//endfor
-
 	}
 
 	else {
-
 		drawInitiativeLabels();
-
 		for (var i = 0; i < geocodesData.length; i++) {
-
 			var cname = geocodesData[i].name;
-
 			cname = cname.toLowerCase();
-
 			if (cname.split(" ")[0] == "saint") {
-
 				cname = cname.replace(" ", "");
-
 			}
-
 			else cname = cname.split(" ")[0];
-
 			map.addLayer(window[cname + "initiatives"]);
-
 		}//endfor
-
 	}
-	
 }
 
 L.NumberedDivIcon = L.Icon.extend({
 	options: {
-		// EDIT THIS TO POINT TO THE FILE AT http://www.charliecroom.com/marker_hole.png (or your own marker)
 		iconUrl: 'http://www.charliecroom.com/marker_hole.png',
 		number: '',
 		shadowUrl: null,
 		iconSize: new L.Point(25, 41),
 		iconAnchor: new L.Point(13, 41),
 		popupAnchor: new L.Point(0, -33),
-		/*
-		 iconAnchor: (Point)
-		 popupAnchor: (Point)
-		 */
 		className: 'leaflet-div-icon'
 	},
 
@@ -266,18 +171,6 @@ L.NumberedDivIcon = L.Icon.extend({
 	}
 });
 
-/***  little hack starts here ***/
-L.Map = L.Map.extend({
-	openPopup: function(popup) {
-		//        this.closePopup();  // just comment this
-		this._popup = popup;
-
-		return this.addLayer(popup).fire('popupopen', {
-			popup: this._popup
-		});
-	}
-}); /***  end of hack ***/
-
 var map = L.map('map',
 		{closePopupOnClick : false}).setView([17.96, -71.09], 5);
 L.tileLayer(maptileurl + access_token, {
@@ -289,133 +182,91 @@ L.tileLayer(maptileurl + access_token, {
 getEverything('');
 
 function mapReady ( ){
-
 	$( ".accordion" ).accordion({
 		collapsible: true,
 		active: 2
 	});
-
+	
 	$(".country-boxes").click(function (event) {
-
 		var layerToRemove = event.target.id;
-
 		if (layerToRemove.split(" ")[0] == 'saint') layerToRemove = layerToRemove.split(" ")[0]+layerToRemove.split(" ")[1];
-
 		else if (layerToRemove.split(" ")[1] == 'and') layerToRemove = layerToRemove.split(" ")[0];
-		
 		else layerToRemove = layerToRemove.split(" ")[0];
-
 		window[layerToRemove + 'Toggle'] = !window[layerToRemove + 'Toggle'];
-
 		handleSearchInput();
-
 	});
 
 	$(".type-boxes").click(function (event) {
-
 		for (var i = 0; i < stakeholderCatArr.length; i++) {
-
 			if (event.target.id == stakeholderCatArr[i]) window[stakeholderCatArr[i].replace(/\W/g, '') + "Toggle"] = !window[stakeholderCatArr[i].replace(/\W/g, '') + "Toggle"];
-
 		}
-
 		handleSearchInput();
-
 	});
 
 	$(".category-boxes").click(function (event) {
-
 		for (var i = 0; i < initiativeCatArr.length; i++) {
-
 			if (event.target.id == initiativeCatArr[i]) window[initiativeCatArr[i].replace(/\W/g, '') + "Toggle"] = !window[initiativeCatArr[i].replace(/\W/g, '') + "Toggle"];
-
 		}
-
 		handleSearchInput();
-
 	});
-
 }
 
 function shouldTypeBeDrawn (marker_type) { //this function will return true if a marker should be drawn on the map based on its type or false otherwise
-
 	var bool = false; // this will check to see if they're all false in which case all types will be rendered
 	
 	for (var i = 0; i < stakeholderCatArr.length; i++) {
-
 		if (window[stakeholderCatArr[i].replace(/\W/g, '') + "Toggle"]) bool = true;
-
 	}
 	
 	if (!bool) return true;//if still false - it means they're all false so we return true
 	
 	for (var i = 0; i < stakeholderCatArr.length; i++) {
-
 		if (marker_type == stakeholderCatArr[i]) return window[stakeholderCatArr[i].replace(/\W/g, '') + "Toggle"];
-
 	}
 
 	return true;
-
 }
 
 function shouldCategoryBeDrawn (marker_type) { //this function will return true if a marker should be drawn on the map based on its type or false otherwise
-
 	var bool = false; // this will check to see if they're all false in which case all types will be rendered
-
 	for (var i = 0; i < initiativeCatArr.length; i++) {
-
 		if (window[initiativeCatArr[i].replace(/\W/g, '') + "Toggle"]) bool = true;
-
 	}
 
 	if (!bool) return true;//if still false - it means they're all false so we return true
 
 	for (var i = 0; i < initiativeCatArr.length; i++) {
-
 		if (marker_type == initiativeCatArr[i]) return window[initiativeCatArr[i].replace(/\W/g, '') + "Toggle"];
-
 	}
 
 	return true;
-
 }
 
 function shouldCountryBeDrawn (country_name) { //this function will return true if a marker should be drawn on the map based on its type or false otherwise
-
 	var ret = false;
 
 	for (var i = 0; i < countryArr.length; i++) {
-
 		if (window[countryArr[i] + "Toggle"]) ret = true;
-
 	}//endfor
 	
 	if (!ret) return true; //if none of the boxes is checked return true
 	
 	for (var i = 0; i < countryArr.length; i++) {
-
 		map.removeLayer(window[countryArr[i]]);
-
 		map.removeLayer(window[countryArr[i] + "initiatives"]);
-
 	}//endfor
 	
 	var toggleName = countryNameParse (country_name).toLowerCase().replace(/\s+/g, '');
 
 	return window[toggleName + 'Toggle'];
-
 }
 
 
 function infoSlideDown (element) {
-
 	var subElement = $(element).next().slideToggle("fast");
-
+	
 	if(element.childNodes[0].childNodes[0].innerHTML == "+") element.childNodes[0].childNodes[0].innerHTML = '-';
-
 	else if(element.childNodes[0].childNodes[0].innerHTML == "-") element.childNodes[0].childNodes[0].innerHTML = '+';
-
 }
 
 var options = {
@@ -439,154 +290,99 @@ document.getElementById('search-box').onkeypress = function(e){
 }
 
 function resetSearchBox () {
-
 	document.getElementById('search-box').value = '';
-
 	handleSearchInput();
-
 }
 
 function resetCountryFilters () {
-
 	$(".country-boxes").each(function(){
-
 		for (var i = 0; i < countryArr.length; i++) {
-
 			window[countryArr[i] + "Toggle"] = false;
-
 		}//endfor
 		
 		if ($(this).prop("checked")) {
-
 			$(this).prop('checked', false);
-
 		}
 	});
-
 }
 
 function resetTypeFilters () {
-
 	for (var i = 0; i < stakeholderCatArr.length; i++) {
-
 		window[stakeholderCatArr[i].replace(/\W/g, '') + "Toggle"] = false;
-
 	}
 
 	$(".type-boxes").each(function(){
-
 		if ($(this).prop("checked")) {
-
 			$(this).prop('checked', false);
-
 		}
 	});
-
 }
 
 function resetCategoryFilters () {
-
 	for (var i = 0; i < initiativeCatArr.length; i++) {
-
 		window[initiativeCatArr[i].replace(/\W/g, '') + "Toggle"] = false;
-
 	}
 	
 	$(".category-boxes").each(function(){
 
 		if ($(this).prop("checked")) {
-
 			$(this).prop('checked', false);
-
 		}
 	});
-
 }
 
 function handleSearchInput () {
-
 	for (var i = 0; i < countryArr.length; i++) {
-		
 		map.removeLayer(window[countryArr[i]]);
-
 		map.removeLayer(window[countryArr[i] + "initiatives"]);
-
 	}//endfor
 
 	loadCountries();
-
 	getEverything(document.getElementById('search-box').value);
-
 }
 
 function putLayersOnMap () {
-
 	if (stakeholdersToggle) {
-
 		drawLabels();
 
 		for (var i = 0; i < countryArr.length; i++) {
-
 			map.addLayer(window[countryArr[i]]);
-
 		}//endfor
 	}	
 	
 	else {
-
 		drawInitiativeLabels();
 
 		for (var i = 0; i < countryArr.length; i++) {
-
 			map.addLayer(window[countryArr[i] + "initiatives"]);
-
 		}//endfor
-		
-	}	
-
+	}
 }
 
 $(".mapToggle").click(function(){
-	
 	if (!$(this).parent().hasClass( "active" )) {
-
 		stakeholdersToggle = !stakeholdersToggle;
 
 		if (stakeholdersToggle) {
-
 			$("#stakeholders_only").show();
-
 			$("#initiatives_only").hide();
-
 		}
-
 		else {
-
 			$("#stakeholders_only").hide();
-
 			$("#initiatives_only").show();
-
 		}
-
 		handleSearchInput();
-
-	}	
-	
+	}
 });
 
 jQuery(document).ready(function() {
-
 	$("#initiatives_only").hide();
-	
 	jQuery('.tabs .tab-links a').on('click', function(e)  {
 		var currentAttrValue = jQuery(this).attr('href');
-
 		// Show/Hide Tabs
 		jQuery('.tabs ' + currentAttrValue).show().siblings().hide();
-
 		// Change/remove current tab to active
 		jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
-
 		e.preventDefault();
 	});
 });
